ad67a5471f606af8931837d1c4ae29de5dddca4d
charlesgm <carlos.masciarelli@bitlogic.io>
2018-09-02 23:21:05 -0300

    Add copyright note

--- cmscraper/devices/technicolor/dpc3848ve.py
+++ cmscraper/devices/technicolor/dpc3848ve.py
@@ -18,7 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# from __future__ import absolute_import
 from cmscraper.scraper import Scraper, Statistics, ChannelStatistics
 from bs4 import BeautifulSoup
 from os import getenv
