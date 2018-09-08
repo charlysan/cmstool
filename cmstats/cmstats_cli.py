eec3bd8e9a8f1cced7b2674f1e420f06017c5fd1
charlesgm <carlos.masciarelli@bitlogic.io>
2018-09-08 01:00:42 -0300

    Code styling

--- cmstats/cmstats_cli.py
+++ cmstats/cmstats_cli.py
@@ -18,19 +18,17 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from mpld3._server import serve
+from mpld3 import fig_to_html
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 import datetime
 import time
 import os
-from mpld3._server import serve
-from mpld3 import fig_to_html
 
 
 def convertfunc(x): return datetime.datetime.fromtimestamp(int(x))
-
-
-import argparse
 
 
 def main():
@@ -46,6 +44,21 @@ def main():
 
     parser.add_argument('--chr', action='store', dest='CHANNEL_RANGE', default=None,
                         type=int, nargs='+', help='Plot a range of channels (e.g. 0 24)')
+
+    parser.add_argument('--path', action='store', dest='DATA_PATH', default='./data',
+                        type=str, help='Absolute path containing csv files (default: ./data)')
+
+    parser.add_argument('--since', action='store', dest='SINCE_DATETIME', default=None,
+                        type=str, help='Since datetime [format: Y-M-D H:M:S] (e.g. --since "2018-09-25 08:00:00")')
+
+    parser.add_argument('--till', action='store', dest='TILL_DATETIME', default=None,
+                        type=str, help='Till datetime [format: Y-M-D H:M:S] (e.g. --till "2018-09-28 18:00:00")')
+
+    parser.add_argument('--ip', action='store', dest='IP', default='0.0.0.0',
+                        type=str, help='Ip address at which the HTML will be served (default: 0.0.0.0)')
+
+    parser.add_argument('--do_not_open_browser', action='store_false', dest='DO_NOT_OPEN_BROWSER',
+                        help='Do not open a web browser after processing input')
 
     args = parser.parse_args()
 
@@ -65,42 +78,90 @@ def main():
         for i in range(args.CHANNEL_RANGE[0], args.CHANNEL_RANGE[-1] + 1):
             channels.append(i)
 
-    plot(channels)
+    plot(
+        channels=channels,
+        path=args.DATA_PATH,
+        datetime_start=args.SINCE_DATETIME,
+        datetime_end=args.TILL_DATETIME,
+        ip=args.IP,
+        open_browser=args.DO_NOT_OPEN_BROWSER)
 
 
-def plot(channels=[0]):
-    plt.rcParams.update({'figure.max_open_warning': 0})
+def plot(channels=[0], path='./data', datetime_start=None,
+         datetime_end=None, ip='0.0.0.0', open_browser=True):
+    '''Plot Power/SNR vs time for all the channels specified within channels list'''
 
-    FIG_SIZE_X = 12
-    FIG_SIZE_Y = 7
-    PLOT_POWER_LINE_WIDTH = 1.2
-    PLOT_POWER_COLOR = 'blue'
-    PLOT_SNR_LINE_WIDTH = 1.2
-    PLOT_SNR_COLOR = 'red'
+    FIG_SIZE_X = os.getenv('FIG_SIZE_X', 12)
+    FIG_SIZE_Y = os.getenv('FIG_SIZE_Y', 7)
+    PLOT_TITLE_FONT_SIZE = os.getenv('PLOT_TITLE_FONT_SIZE', 12)
+    PLOT_POWER_LINE_WIDTH = os.getenv('PLOT_POWER_LINE_WIDTH', 1.2)
+    PLOT_POWER_COLOR = os.getenv('PLOT_POWER_COLOR', 'blue')
+    PLOT_SNR_LINE_WIDTH = os.getenv('PLOT_SNR_LINE_WIDTH', 1.2)
+    PLOT_SNR_COLOR = os.getenv('PLOT_SNR_COLOR', 'red')
 
-    SNR_MIN_THRESHOLD = 30  # For limiting SNR y axis plot
-    SNR_MIN_Y_VALUE_1 = 32
-    SNR_MAX_Y_VALUE_1 = 40
-    SNR_MIN_Y_VALUE_2 = 5
-    SNR_MAX_Y_VALUE_2 = 40
+    SNR_MIN_THRESHOLD = os.getenv('SNR_MIN_THRESHOLD', 30) # For limiting SNR y axis plot
+    SNR_MIN_Y_VALUE_1 = os.getenv('SNR_MIN_Y_VALUE_1', 32)
+    SNR_MAX_Y_VALUE_1 = os.getenv('SNR_MAX_Y_VALUE_1', 40)
+    SNR_MIN_Y_VALUE_2 = os.getenv('SNR_MIN_Y_VALUE_2', 5)
+    SNR_MAX_Y_VALUE_2 = os.getenv('SNR_MAX_Y_VALUE_2', 40)
+
+    datetime_format = '%Y-%m-%d %H:%M:%S'
 
     figures = dict()
 
-    for x in channels:
-        f = './data/' + str(x) + '.csv'
+     # Do not print warning if more than 20 plots are opened
+    plt.rcParams.update({'figure.max_open_warning': 0})
+
+    print('')
+    for c in channels:
+        f = path + '/' + str(c) + '.csv'
         exists = os.path.isfile(f)
         if not exists:
             continue
 
-        data = np.genfromtxt(f, delimiter=',', skip_header=0)
-        start = 1
-        end = 10
+        data = np.genfromtxt(f, delimiter=',')
+
+        # Crop input based on start/end datetime
+        start = None
+        end = None
+
+        if datetime_start is not None or datetime_end is not None:
+            if datetime_start is not None:
+                timestamp_from = int(
+                    time.mktime(
+                        datetime.datetime.strptime(
+                            datetime_start,
+                            datetime_format).timetuple()))
+            if datetime_end is not None:
+                timestamp_to = int(
+                    time.mktime(
+                        datetime.datetime.strptime(
+                            datetime_end,
+                            datetime_format).timetuple()))
+
+            for i, line in enumerate(data):
+                if datetime_start is not None and line[0] >= timestamp_from:
+                    if start is None:
+                        start = i
+                if datetime_end is not None and line[0] >= timestamp_to:
+                    if end is None:
+                        end = i
 
         timestamp = np.genfromtxt(
-            f, delimiter=',', unpack=True, converters={
-                0: convertfunc}, usecols=0)
-        power = np.genfromtxt(f, delimiter=',', unpack=True, usecols=1)
-        snr = np.genfromtxt(f, delimiter=',', unpack=True, usecols=2)
+            f, delimiter = ',', unpack=True, converters={0: convertfunc},
+            skip_header=start if start is not None else 1,
+            skip_footer=(len(data) - end) if end is not None else 0,
+            usecols=0)
+        power = np.genfromtxt(
+            f, delimiter = ',', unpack=True,
+            skip_header = start if start is not None else 1,
+            skip_footer = (len(data) - end) if end is not None else 0,
+            usecols=1)
+        snr = np.genfromtxt(
+            f, delimiter = ',', unpack=True,
+            skip_header = start if start is not None else 1,
+            skip_footer = (len(data) - end) if end is not None else 0,
+            usecols = 2)
 
         avg_snr = sum(snr) / float(len(snr))
         avg_pwr = sum(power) / float(len(power))
@@ -108,27 +169,27 @@ def plot(channels=[0]):
         sd_pwr = np.std(np.array(power))
 
         # Print statistics to STDOUT
-        print('Ch ' + str("{:02d}".format(x)) + ': PWR avg: ' + str("{:05.2f}".format(avg_pwr)) +
+        print('Ch ' + str("{:02d}".format(c)) + ': PWR avg: ' + str("{:05.2f}".format(avg_pwr)) +
               ' dBmV / PWR std: ' + str("{:05.2f}".format(sd_pwr)) +
               ' - SNR avg: ' + str("{:05.2f}".format(avg_snr)) +
               ' dB  / SNR std: ' + str("{:05.2f}".format(sd_snr)))
 
-        figures[x] = plt.figure(x, figsize=(FIG_SIZE_X, FIG_SIZE_Y))
+        figures[c] = plt.figure(c, figsize=(FIG_SIZE_X, FIG_SIZE_Y))
 
         # Power Plot
-        ax1 = plt.subplot(211)  # 2 rows, 1 column, subplot #1
+        plt.subplot(211)  # 2 rows, 1 column, subplot #1
         plt.plot(
             timestamp,
             power,
             linewidth=PLOT_POWER_LINE_WIDTH,
             color=PLOT_POWER_COLOR)
-        plt.title('Channel ' + str(x), fontsize=22)
+        plt.title('Channel ' + str(c), fontsize=PLOT_TITLE_FONT_SIZE)
         plt.ylabel('Power (dBmV)')
         plt.xlabel('datetime')
         plt.grid(True)
 
         # SNR Plot
-        ax2 = plt.subplot(212)  # 2 rows, 1 column, subplot #2
+        plt.subplot(212)  # 2 rows, 1 column, subplot #2
         plt.plot(
             timestamp,
             snr,
@@ -141,17 +202,17 @@ def plot(channels=[0]):
         # Limit SNR Y axis based on minimum value for better visualization
         axes = plt.gca()
         axes.set_ylim([SNR_MIN_Y_VALUE_1, SNR_MAX_Y_VALUE_1])
-
         if min(snr) < SNR_MIN_THRESHOLD:
             axes.set_ylim([SNR_MIN_Y_VALUE_2, SNR_MAX_Y_VALUE_2])
 
     # Serve all plots in the same html page
     html_sum = ''
-    for x in figures:
-        html = fig_to_html(figures[x])
+    for f in figures:
+        html = fig_to_html(figures[f])
         html_sum = html_sum + html
 
-    serve(html_sum, open_browser=False, ip="0.0.0.0")
+    print('')
+    serve(html_sum, open_browser=open_browser, ip=ip)
 
 
 if __name__ == "__main__":
